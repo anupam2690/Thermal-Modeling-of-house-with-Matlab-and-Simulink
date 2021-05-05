d910e26d34e49e4e794450428537b6b6ca395c9e
anupam2690 <76772356+anupam2690@users.noreply.github.com>
2021-05-05 12:20:05 +0200

    Add files via upload

---
+++ Thermal_modeling_house_parameters.m
@@ -0,0 +1,14 @@
+%House
+% Total Thermal Resistance
+Req = 0.0000004; % C/J-hr
+%Mass of air in the house
+M = 5987.7684; % Kg
+%Specific heat capacity of air
+C= 1005.4; % J/KgC
+
+%Heater
+%Heater Cost
+cost = 2.5/(3.6*10^6); % Rs/j
+%Mass flow of the heater
+Mdot = 3600; % kg/hr
+Theater = 50; % Outside C
